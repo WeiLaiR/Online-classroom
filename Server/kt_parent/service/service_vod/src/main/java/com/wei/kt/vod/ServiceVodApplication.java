a349141d37cc60b31d7fcf31603a2025b55579f3
Wei <1397311537@qq.com>
2022-08-27 12:03:19 +0800

    导入包

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/ServiceVodApplication.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/ServiceVodApplication.java
@@ -2,8 +2,11 @@ package com.wei.kt.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.wei")
 public class ServiceVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceVodApplication.class, args);
