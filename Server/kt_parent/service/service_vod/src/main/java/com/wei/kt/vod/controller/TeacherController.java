6c85a10e3fa6ce4a1c5e886baaf0d9b3f68aee06
Wei <1397311537@qq.com>
2022-08-27 12:02:32 +0800

    编写TeacherController增加接口

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/controller/TeacherController.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/controller/TeacherController.java
@@ -1,13 +1,18 @@
 package com.wei.kt.vod.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wei.kt.model.vod.Teacher;
+import com.wei.kt.result.Result;
+import com.wei.kt.vo.vod.TeacherQueryVo;
 import com.wei.kt.vod.service.TeacherService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,11 +40,66 @@ public class TeacherController {
         this.teacherService = teacherService;
     }
 
-    //查询所有讲师列表
+    // http://localhost:8301/admin/vod/teacher/findAll
+    //1 查询所有讲师
+    @ApiOperation("查询所有讲师")
     @GetMapping("findAll")
-    public List<Teacher> findAll(){
+    public Result findAllTeacher() {
+        //调用service方法
         List<Teacher> list = teacherService.list();
-        return list;
+        return Result.ok(list).message("查询数据成功");
+    }
+
+    // remove/1
+    //2 逻辑删除讲师
+    @ApiOperation("逻辑删除讲师")
+    @DeleteMapping("remove/{id}")
+    public Result removeTeacher(@ApiParam(name = "id", value = "ID", required = true)
+                                @PathVariable Long id) {
+        boolean isSuccess = teacherService.removeById(id);
+        if(isSuccess) {
+            return Result.ok(null);
+        } else {
+            return Result.fail(null);
+        }
+    }
+
+
+
+    //条件查询分页列表
+    @ApiOperation(value = "获取分页列表")
+    @PostMapping("{page}/{limit}")
+    public Result index(
+            @ApiParam(name = "page", value = "当前页码", required = true)
+            @PathVariable Long page,
+            @ApiParam(name = "limit", value = "每页记录数", required = true)
+            @PathVariable Long limit,
+            @ApiParam(name = "teacherVo", value = "查询对象", required = false)
+            @RequestBody(required = false) TeacherQueryVo teacherQueryVo) {
+        //创建page对象，传递当前页和每页记录数
+        Page<Teacher> pageParam = new Page<>(page, limit);
+        //获取条件值
+        String name = teacherQueryVo.getName();//讲师名称
+        Integer level = teacherQueryVo.getLevel();//讲师级别
+        String joinDateBegin = teacherQueryVo.getJoinDateBegin();//开始时间
+        String joinDateEnd = teacherQueryVo.getJoinDateEnd();//结束时间
+        //封装条件
+        QueryWrapper<Teacher> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(name)) {
+            wrapper.like("name",name);
+        }
+        if(!StringUtils.isEmpty(level)) {
+            wrapper.eq("level",level);
+        }
+        if(!StringUtils.isEmpty(joinDateBegin)) {
+            wrapper.ge("join_date",joinDateBegin);
+        }
+        if(!StringUtils.isEmpty(joinDateEnd)) {
+            wrapper.le("join_date",joinDateEnd);
+        }
+        //调用方法得到分页查询结果
+        IPage<Teacher> pageModel = teacherService.page(pageParam, wrapper);
+        return Result.ok(pageModel);
     }
 }
 
