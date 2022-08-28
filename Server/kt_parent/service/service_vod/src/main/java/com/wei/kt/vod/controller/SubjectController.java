594068a562bfce592aa0251784902b69c3fa50fd
Wei <1397311537@qq.com>
2022-08-28 18:40:27 +0800

    调用Excel导入导出接口

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/controller/SubjectController.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/controller/SubjectController.java
@@ -7,7 +7,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Api(tags = "课程分类管理")
@@ -30,5 +32,23 @@ public class SubjectController {
     public Result getChildSubject(@PathVariable Long id) {
         List<Subject> list = subjectService.selectList(id);
         return Result.ok(list);
+    }
+
+
+
+
+    @ApiOperation(value="导出")
+    @GetMapping(value = "/exportData")
+    public void exportData(HttpServletResponse response) {
+        subjectService.exportData(response);
+    }
+
+
+
+    @ApiOperation(value = "导入")
+    @PostMapping("importData")
+    public Result importData(MultipartFile file) {
+        subjectService.importDictData(file);
+        return Result.ok();
     }
 }
