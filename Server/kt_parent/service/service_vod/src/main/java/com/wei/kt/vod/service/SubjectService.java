276f272f213dcef88cb6da0edb296737e973bd27
Wei <1397311537@qq.com>
2022-08-28 18:39:53 +0800

    创建Excel导入导出接口

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/SubjectService.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/SubjectService.java
@@ -2,11 +2,21 @@ package com.wei.kt.vod.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.wei.kt.model.vod.Subject;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public interface SubjectService extends IService<Subject> {
 
     //查询下一层课程分类
     List<Subject> selectList(Long id);
+
+    /**
+     * 导出
+     * @param response
+     */
+    void exportData(HttpServletResponse response);
+
+    void importDictData(MultipartFile file);
 }
