3fa42089ad4f4445fdb72d6fc47d2e07ee2533e4
Wei <1397311537@qq.com>
2022-08-28 17:09:52 +0800

    新增subjectMapper

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/SubjectMapper.java
@@ -0,0 +1,7 @@
+package com.wei.kt.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.kt.model.vod.Subject;
+
+public interface SubjectMapper extends BaseMapper<Subject> {
+}
