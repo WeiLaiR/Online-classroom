89285b6113671f139ed22611c2383c64b915b8a8
Wei <1397311537@qq.com>
2022-08-28 18:40:00 +0800

    实现Excel导入导出接口

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/impl/SubjectServiceImpl.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/impl/SubjectServiceImpl.java
@@ -1,16 +1,34 @@
 package com.wei.kt.vod.service.impl;
 
+import com.alibaba.excel.EasyExcel;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wei.kt.exception.KtException;
 import com.wei.kt.model.vod.Subject;
+import com.wei.kt.vo.vod.SubjectEeVo;
+import com.wei.kt.vod.listaner.SubjectListener;
 import com.wei.kt.vod.mapper.SubjectMapper;
 import com.wei.kt.vod.service.SubjectService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> implements SubjectService {
+
+
+    private SubjectListener subjectListener;
+    @Autowired
+    public void setSubjectListener(SubjectListener subjectListener) {
+        this.subjectListener = subjectListener;
+    }
 
     //查询下一层课程分类
     @Override
@@ -26,6 +44,39 @@ public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> impl
         }
         return subjectList;
     }
+
+    //课程分类导出
+    @Override
+    public void exportData(HttpServletResponse response) {
+        try {
+            response.setContentType("application/vnd.ms-excel");
+            response.setCharacterEncoding("utf-8");
+            // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+            String fileName = URLEncoder.encode("课程分类", "UTF-8");
+            response.setHeader("Content-disposition", "attachment;filename="+ fileName + ".xlsx");
+            List<Subject> dictList = baseMapper.selectList(null);
+            List<SubjectEeVo> dictVoList = new ArrayList<>(dictList.size());
+            for(Subject dict : dictList) {
+                SubjectEeVo dictVo = new SubjectEeVo();
+                BeanUtils.copyProperties(dict,dictVo);
+                dictVoList.add(dictVo);
+            }
+            EasyExcel.write(response.getOutputStream(), SubjectEeVo.class).sheet("课程分类").doWrite(dictVoList);
+        } catch (IOException e) {
+            throw new KtException(20001,"导出失败");
+        }
+    }
+
+    @Override
+    public void importDictData(MultipartFile file) {
+        try {
+            EasyExcel.read(file.getInputStream(),
+                    SubjectEeVo.class,subjectListener).sheet().doRead();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //判断id下面是否有子节点
     private boolean isChildren(Long id) {
         QueryWrapper<Subject> wrapper = new QueryWrapper<>();
