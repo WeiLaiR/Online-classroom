791f042d5e921966770f7ab14c245b57fc2d3991
Wei <1397311537@qq.com>
2022-08-28 18:39:16 +0800

    注入

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/SubjectMapper.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/SubjectMapper.java
@@ -2,6 +2,8 @@ package com.wei.kt.vod.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wei.kt.model.vod.Subject;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubjectMapper extends BaseMapper<Subject> {
 }
