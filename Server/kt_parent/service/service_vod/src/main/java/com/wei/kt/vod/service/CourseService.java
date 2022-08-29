f9b734de5b8bffb5fb0ea64d2061096c6e183eb4
Wei <1397311537@qq.com>
2022-08-29 10:29:23 +0800

    创建并实现课程管理接口

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/CourseService.java
@@ -0,0 +1,13 @@
+package com.wei.kt.vod.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wei.kt.model.vod.Course;
+import com.wei.kt.vo.vod.CourseQueryVo;
+
+import java.util.Map;
+
+public interface CourseService extends IService<Course> {
+    //课程列表
+    Map<String,Object> findPage(Page<Course> pageParam, CourseQueryVo courseQueryVo);
+}
---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/impl/CourseServiceImpl.java
@@ -0,0 +1,91 @@
+package com.wei.kt.vod.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wei.kt.model.vod.Course;
+import com.wei.kt.model.vod.Subject;
+import com.wei.kt.model.vod.Teacher;
+import com.wei.kt.vo.vod.CourseQueryVo;
+import com.wei.kt.vod.mapper.CourseMapper;
+import com.wei.kt.vod.service.CourseService;
+import com.wei.kt.vod.service.SubjectService;
+import com.wei.kt.vod.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CourseServiceImpl extends ServiceImpl<CourseMapper, Course> implements CourseService {
+
+    @Autowired
+    private TeacherService teacherService;
+
+    @Autowired
+    private SubjectService subjectService;
+
+    //课程列表
+    @Override
+    public Map<String,Object> findPage(Page<Course> pageParam, CourseQueryVo courseQueryVo) {
+        //获取条件值
+        String title = courseQueryVo.getTitle();//名称
+        Long subjectId = courseQueryVo.getSubjectId();//二级分类
+        Long subjectParentId = courseQueryVo.getSubjectParentId();//一级分类
+        Long teacherId = courseQueryVo.getTeacherId();//讲师
+        //封装条件
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        if(!StringUtils.isEmpty(title)) {
+            wrapper.like("title",title);
+        }
+        if(!StringUtils.isEmpty(subjectId)) {
+            wrapper.eq("subject_id",subjectId);
+        }
+        if(!StringUtils.isEmpty(subjectParentId)) {
+            wrapper.eq("subject_parent_id",subjectParentId);
+        }
+        if(!StringUtils.isEmpty(teacherId)) {
+            wrapper.eq("teacher_id",teacherId);
+        }
+        //调用方法查询
+        Page<Course> pages = baseMapper.selectPage(pageParam, wrapper);
+        long totalCount = pages.getTotal();//总记录数
+        long totalPage = pages.getPages();//总页数
+        long currentPage = pages.getCurrent();//当前页
+        long size = pages.getSize();//每页记录数
+        //每页数据集合
+        List<Course> records = pages.getRecords();
+        //遍历封装讲师和分类名称
+        records.stream().forEach(item -> {
+            this.getTeacherOrSubjectName(item);
+        });
+        //封装返回数据
+        Map<String,Object> map = new HashMap<>();
+        map.put("totalCount",totalCount);
+        map.put("totalPage",totalPage);
+        map.put("records",records);
+        return map;
+    }
+
+    //获取讲师和分类名称
+    private Course getTeacherOrSubjectName(Course course) {
+        //查询讲师名称
+        Teacher teacher = teacherService.getById(course.getTeacherId());
+        if(teacher != null) {
+            course.getParam().put("teacherName",teacher.getName());
+        }
+        //查询分类名称
+        Subject subjectOne = subjectService.getById(course.getSubjectParentId());
+        if(subjectOne != null) {
+            course.getParam().put("subjectParentTitle",subjectOne.getTitle());
+        }
+        Subject subjectTwo = subjectService.getById(course.getSubjectId());
+        if(subjectTwo != null) {
+            course.getParam().put("subjectTitle",subjectTwo.getTitle());
+        }
+        return course;
+    }
+}
