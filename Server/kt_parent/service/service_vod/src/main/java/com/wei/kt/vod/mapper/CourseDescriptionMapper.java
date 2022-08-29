e1ac6645c02162feed9879231788d00223e3498f
Wei <1397311537@qq.com>
2022-08-29 11:26:43 +0800

    实现添加课程Mapper

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/CourseDescriptionMapper.java
@@ -0,0 +1,9 @@
+package com.wei.kt.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.kt.model.vod.CourseDescription;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseDescriptionMapper extends BaseMapper<CourseDescription> {
+}
