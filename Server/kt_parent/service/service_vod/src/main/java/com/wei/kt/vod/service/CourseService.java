7b04ee01090d96170c5466042f3503b529c63238
Wei <1397311537@qq.com>
2022-08-29 11:27:14 +0800

    新增添加课程

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/CourseService.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/CourseService.java
@@ -3,11 +3,15 @@ package com.wei.kt.vod.service;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.wei.kt.model.vod.Course;
+import com.wei.kt.vo.vod.CourseFormVo;
 import com.wei.kt.vo.vod.CourseQueryVo;
 
 import java.util.Map;
 
 public interface CourseService extends IService<Course> {
+
+    Long saveCourseInfo(CourseFormVo courseFormVo);
+
     //课程列表
     Map<String,Object> findPage(Page<Course> pageParam, CourseQueryVo courseQueryVo);
 }
