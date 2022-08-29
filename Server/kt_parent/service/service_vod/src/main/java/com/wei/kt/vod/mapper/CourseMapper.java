52ce28b5a6ca1e792ca2f8e6b9cc2804099fe9cc
Wei <1397311537@qq.com>
2022-08-29 10:29:00 +0800

    新增点播管理Mapper

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/CourseMapper.java
@@ -0,0 +1,9 @@
+package com.wei.kt.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.kt.model.vod.Course;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMapper extends BaseMapper<Course> {
+}
