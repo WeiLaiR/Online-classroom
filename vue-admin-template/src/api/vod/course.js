65ec4554738f20936074d0da3275b5cb20c29a76
Wei <1397311537@qq.com>
2022-08-29 16:37:35 +0800

    新增id获取课程信息、修改课程信息请求

--- vue-admin-template/src/api/vod/course.js
+++ vue-admin-template/src/api/vod/course.js
@@ -20,4 +20,20 @@ export default {
         data: courseInfo
         })
     },
+
+      //id获取课程信息
+    getCourseInfoById(id) {
+        return request({
+        url: `${api_name}/get/${id}`,
+        method: 'get'
+        })
+    },
+    //修改课程信息
+    updateCourseInfoById(courseInfo) {
+        return request({
+        url: `${api_name}/update`,
+        method: 'put',
+        data: courseInfo
+        })
+    },
 }
