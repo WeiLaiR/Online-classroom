fa598a896fe0ca8a622d8195511d0f640df95ffa
Wei <1397311537@qq.com>
2022-09-01 17:59:08 +0800

    新建ChapterServiceImpl，实现章节小结列表接口

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/impl/ChapterServiceImpl.java
@@ -0,0 +1,66 @@
+package com.wei.kt.vod.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.wei.kt.model.vod.Chapter;
+import com.wei.kt.model.vod.Video;
+import com.wei.kt.vo.vod.ChapterVo;
+import com.wei.kt.vo.vod.VideoVo;
+import com.wei.kt.vod.mapper.ChapterMapper;
+import com.wei.kt.vod.service.ChapterService;
+import com.wei.kt.vod.service.VideoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChapterServiceImpl extends ServiceImpl<ChapterMapper, Chapter> implements ChapterService {
+
+    @Autowired
+    private VideoService videoService;
+
+    //章节小结列表封装
+    @Override
+    public List<ChapterVo> getNestedTreeList(Long courseId) {
+        List<ChapterVo> chapterVoList = new ArrayList<>();
+
+        //获取章信息
+        LambdaQueryWrapper<Chapter> queryWrapperChapter = new LambdaQueryWrapper<>();
+        queryWrapperChapter.eq(Chapter::getCourseId, courseId);
+        queryWrapperChapter.orderByAsc(Chapter::getSort, Chapter::getId);
+        List<Chapter> chapterList = baseMapper.selectList(queryWrapperChapter);
+
+        //获取课时信息
+        LambdaQueryWrapper<Video> queryWrapperVideo = new LambdaQueryWrapper<>();
+        queryWrapperVideo.eq(Video::getCourseId, courseId);
+        queryWrapperVideo.orderByAsc(Video::getSort, Video::getId);
+        List<Video> videoList = videoService.list(queryWrapperVideo);
+
+        //填充列表数据：Chapter列表
+        for (int i = 0; i < chapterList.size(); i++) {
+            Chapter chapter = chapterList.get(i);
+
+            //创建ChapterVo对象
+            ChapterVo chapterVo = new ChapterVo();
+            BeanUtils.copyProperties(chapter, chapterVo);
+            chapterVoList.add(chapterVo);
+
+            //填充列表数据：Video列表
+            List<VideoVo> videoVoList = new ArrayList<>();
+            for (int j = 0; j < videoList.size(); j++) {
+                Video video = videoList.get(j);
+                if(chapter.getId().equals(video.getChapterId())){
+
+                    VideoVo videoVo = new VideoVo();
+                    BeanUtils.copyProperties(video, videoVo);
+                    videoVoList.add(videoVo);
+                }
+            }
+            chapterVo.setChildren(videoVoList);
+        }
+        return chapterVoList;
+    }
+}
