a258dd51a9e950fd200d5fb5acec89e6a426156a
Wei <1397311537@qq.com>
2022-09-01 17:58:30 +0800

    新建ChapterMapper

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/ChapterMapper.java
@@ -0,0 +1,7 @@
+package com.wei.kt.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.kt.model.vod.Chapter;
+
+public interface ChapterMapper extends BaseMapper<Chapter> {
+}
