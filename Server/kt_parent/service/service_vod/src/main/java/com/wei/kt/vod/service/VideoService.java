a7dfa2eb1cb8985b60061c52d14d571f133a0ea7
Wei <1397311537@qq.com>
2022-09-01 18:02:25 +0800

    创建VideoService

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/VideoService.java
@@ -0,0 +1,7 @@
+package com.wei.kt.vod.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wei.kt.model.vod.Video;
+
+public interface VideoService extends IService<Video> {
+}
