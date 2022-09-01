ab0f7c391bc04740118db5ff1562f003d731bff8
Wei <1397311537@qq.com>
2022-09-01 18:00:14 +0800

    新增根据id获取课程发布信息

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/CourseMapper.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/CourseMapper.java
@@ -2,8 +2,11 @@ package com.wei.kt.vod.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.wei.kt.model.vod.Course;
+import com.wei.kt.vo.vod.CoursePublishVo;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseMapper extends BaseMapper<Course> {
+    //根据id获取课程发布信息
+    CoursePublishVo selectCoursePublishVoById(Long id);
 }
