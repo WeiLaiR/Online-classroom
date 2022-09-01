653e26ce819e54eae4e8935f17f9931ffd7edea1
Wei <1397311537@qq.com>
2022-09-01 18:02:15 +0800

    创建VideoMapper

---
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/mapper/VideoMapper.java
@@ -0,0 +1,7 @@
+package com.wei.kt.vod.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wei.kt.model.vod.Video;
+
+public interface VideoMapper extends BaseMapper<Video> {
+}
