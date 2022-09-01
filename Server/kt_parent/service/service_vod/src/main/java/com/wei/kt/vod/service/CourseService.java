974b39bf6d0470356269eddea5c2f7c60e190772
Wei <1397311537@qq.com>
2022-09-01 18:00:52 +0800

    创建根据id获取课程发布信息、根据id发布课程接口

--- Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/CourseService.java
+++ Server/kt_parent/service/service_vod/src/main/java/com/wei/kt/vod/service/CourseService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.wei.kt.model.vod.Course;
 import com.wei.kt.vo.vod.CourseFormVo;
+import com.wei.kt.vo.vod.CoursePublishVo;
 import com.wei.kt.vo.vod.CourseQueryVo;
 
 import java.util.Map;
@@ -20,4 +21,10 @@ public interface CourseService extends IService<Course> {
 
     //根据id修改课程信息
     void updateCourseById(CourseFormVo courseFormVo);
+
+    //根据id获取课程发布信息
+    CoursePublishVo getCoursePublishVo(Long id);
+
+    //根据id发布课程
+    boolean publishCourseById(Long id);
 }
